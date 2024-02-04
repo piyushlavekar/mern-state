f1c5b9662d8a73c782d64238729f1e496161a2fa
piyushlavekar <piyushlavekar0905@gmail.com>
2024-02-04 06:34:40 +0530

    create pages and routes

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+// Example for Home component
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      {/* Other content */}
+    </div>
+  );
+};
+
+export default Home;
